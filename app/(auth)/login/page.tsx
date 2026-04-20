fe21e3ee88b69cf9b7e3ed726a58de7391e4f2c5
Serhii Kravcnenko <sergkrava1983@gmail.com>
2026-04-20 22:35:18 +0300

    login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>
+        Your medication, delivered Say goodbye to all your healthcare worries
+        with us
+      </h1>
+    </div>
+  );
+}
