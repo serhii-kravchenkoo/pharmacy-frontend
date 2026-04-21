c6c7cb795984a1f65b9f5b8bd088164d264c5479
Serhii Kravcnenko <sergkrava1983@gmail.com>
2026-04-21 23:53:46 +0300

    form login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,50 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
     <div>
       <h1>
         Your medication, delivered Say goodbye to all your healthcare worries
         with us
       </h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            {...register('email', { required: 'Email is required' })}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
+
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            {...register('password', { required: 'Password is required' })}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+        </div>
+
+        <button type="submit">Log in</button>
+      </form>
     </div>
   );
 }
